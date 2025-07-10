2a7c2b398c570871f02aabbf23a15504d12e0ea1
dveo26 <devansh26.01.2005@gmail.com>
2025-07-10 18:34:32 +0530

    ai career coach

---
+++ app/layout.js
@@ -0,0 +1,51 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark,neobrutalism ,shadesOfPurple} from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+        variables: {
+         
+          colorText: "#ffffff",
+          colorBackground: "#210346",
+          colorDanger: "#ff4d4d",
+          colorSuccess: "#28a745",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+           
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
